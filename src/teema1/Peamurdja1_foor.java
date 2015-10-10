1cf112fa94a81669ec2775244ba2e7f73d838bf0
German Breus <german.breus@gmail.com>
2015-10-10 20:52:41 +0300

    Praktikum 10.10.2015 kodune töö

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +19,38 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor = new Foor();
+
+        int count = 0;
+        while (count < 5) {
+            foor.vahetaPunast();
+            foor.paus(2);
+            foor.vahetaKollast();
+            foor.paus(0.6);
+            foor.vahetaPunast();
+            foor.vahetaKollast();
+            foor.vahetaRohelist();
+            foor.paus(3);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.paus(0.5);
+            foor.vahetaRohelist();
+            foor.vahetaKollast();
+            foor.paus(0.8);
+            foor.vahetaKollast();
+            count++;
+        }
+    }
 }
--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,8 +1,133 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lib.Foor;
+
 /**
  * Täpselt nagu oli fooriga, on nüüd ristmikuga. Kuidas esiteks
  * ristmik üles ehitada loe Foori README.md-st: https://github.com/KristerV/javaHarjutused
  */
-public class Peamurdja2_ristmik {
+public class Peamurdja2_ristmik extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor1 = new Foor(Foor.ALUMINE, primaryStage);
+        Foor foor2 = new Foor(Foor.YLEMINE, primaryStage);
+        Foor foor3 = new Foor(Foor.VASAK, primaryStage);
+        Foor foor4 = new Foor(Foor.PAREM, primaryStage);
+
+        int count = 0;
+        while (count < 5) {
+            foor1.vahetaPunast();
+            foor1.paus(2);
+            foor1.vahetaKollast();
+            foor1.paus(0.6);
+            foor1.vahetaPunast();
+            foor1.vahetaKollast();
+            foor1.vahetaRohelist();
+            foor1.paus(3);
+            foor1.vahetaRohelist();
+            foor1.paus(0.5);
+            foor1.vahetaRohelist();
+            foor1.paus(0.5);
+            foor1.vahetaRohelist();
+            foor1.paus(0.5);
+            foor1.vahetaRohelist();
+            foor1.paus(0.5);
+            foor1.vahetaRohelist();
+            foor1.paus(0.5);
+            foor1.vahetaRohelist();
+            foor1.paus(0.5);
+            foor1.vahetaRohelist();
+            foor1.vahetaKollast();
+            foor1.paus(0.8);
+            foor1.vahetaKollast();
+            count++;
+        }
+        int count2 = 0;
+        while (count2 < 5) {
+            foor2.vahetaPunast();
+            foor2.paus(2);
+            foor2.vahetaKollast();
+            foor2.paus(0.6);
+            foor2.vahetaPunast();
+            foor2.vahetaKollast();
+            foor2.vahetaRohelist();
+            foor2.paus(3);
+            foor2.vahetaRohelist();
+            foor2.paus(0.5);
+            foor2.vahetaRohelist();
+            foor2.paus(0.5);
+            foor2.vahetaRohelist();
+            foor2.paus(0.5);
+            foor2.vahetaRohelist();
+            foor2.paus(0.5);
+            foor2.vahetaRohelist();
+            foor2.paus(0.5);
+            foor2.vahetaRohelist();
+            foor2.paus(0.5);
+            foor2.vahetaRohelist();
+            foor2.vahetaKollast();
+            foor2.paus(0.8);
+            foor2.vahetaKollast();
+            count2++;
+        }
+        int count3 = 0;
+        while (count3 < 5) {
+            foor3.vahetaRohelist();
+            foor3.paus(3);
+            foor3.vahetaRohelist();
+            foor3.paus(0.5);
+            foor3.vahetaRohelist();
+            foor3.paus(0.5);
+            foor3.vahetaRohelist();
+            foor3.paus(0.5);
+            foor3.vahetaRohelist();
+            foor3.paus(0.5);
+            foor3.vahetaRohelist();
+            foor3.paus(0.5);
+            foor3.vahetaRohelist();
+            foor3.paus(0.5);
+            foor3.vahetaRohelist();
+            foor3.vahetaKollast();
+            foor3.paus(0.8);
+            foor3.vahetaKollast();
+            foor3.vahetaPunast();
+            foor3.paus(2);
+            foor3.vahetaKollast();
+            foor3.paus(0.6);
+            foor3.vahetaPunast();
+            foor3.vahetaKollast();
+            count3++;
+        }
+        int count4 = 0;
+        while (count4 < 5) {
+            foor4.vahetaRohelist();
+            foor4.paus(3);
+            foor4.vahetaRohelist();
+            foor4.paus(0.5);
+            foor4.vahetaRohelist();
+            foor4.paus(0.5);
+            foor4.vahetaRohelist();
+            foor4.paus(0.5);
+            foor4.vahetaRohelist();
+            foor4.paus(0.5);
+            foor4.vahetaRohelist();
+            foor4.paus(0.5);
+            foor4.vahetaRohelist();
+            foor4.paus(0.5);
+            foor4.vahetaRohelist();
+            foor4.vahetaKollast();
+            foor4.paus(0.8);
+            foor4.vahetaKollast();
+            foor4.vahetaPunast();
+            foor4.paus(2);
+            foor4.vahetaKollast();
+            foor4.paus(0.6);
+            foor4.vahetaPunast();
+            foor4.vahetaKollast();
+            count4++;
+        }
+    }
 }
